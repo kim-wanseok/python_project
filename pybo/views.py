3e075bd8b4ed35895c07f33e12a90d4a11ab3d08
wanseok <wanseok.k@gmail.com>
2021-06-09 17:11:00 +0900

    210608 강의코드

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.core import paginator
+from django.forms import fields
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
+from django.views import generic
 from .models import Question, Answer
+from django.utils import timezone
+from .forms import AnswerForm, QuestionForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -8,8 +14,65 @@ def index(request):
     """
     pybo 목록 출력
     """
+
+    page = request.GET.get('page', '1')
+
     question_list = Question.objects.order_by('-create_date')
-    answer_list = Answer.objects.all()
-    context = {'question_list': question_list, 'answer_list': answer_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list':page_obj}
+
+    # answer_list = Answer.objects.all()
+    # context = {'question_list': question_list, 'answer_list': answer_list}
     return render(request, 'pybo/question_list.html', context)
     # return HttpResponse('안녕하세요 학습 동아리 게시판 입니다.')
+
+def detail(request, question_id):
+    """
+    pybo 내용출력
+    """
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+class DetailView(generic.DetailView):
+    model = Question
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'),create_date=timezone.now())
+
+    return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question':question ,'form':form}
+    return render(request, 'pybo/question_detail.html', context)
+
